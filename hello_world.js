3b107fd1e8786260d284914d03953df471a94339
zheng <joyszheng@163.com>
2026-07-23 02:20:17 +0000

    123

--- hello_world.js
+++ hello_world.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.end('Hello World1231233!');
 }).listen(8080,()=>{
   console.log('Server running at http://0.0.0.0:8080/');
 });
